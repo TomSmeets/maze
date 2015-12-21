1a4b8921fdd14f3ef271d96bdd7e5ac60f8931bb
tom <Tom.TSmeets@Gmail.com>
2015-12-21 14:05:51 +0100

    Included some documentation

--- src/Maze.hs
+++ src/Maze.hs
@@ -1,46 +1,72 @@
 {-# LANGUAGE BangPatterns #-}
-module Maze where
+module Maze (renderGrid, genMaze) where
 
 import Control.Monad.State
-import Debug.Trace
-import Linear hiding (trace)
+import Linear
 import System.Random
 
-type Grid = [V2 Integer]
+-- | Alias for the maze generator
 type MazeGen a = State (StdGen, Grid) a
 
-renderGrid :: String -> String -> V2 Integer -> Grid -> [String]
-renderGrid wall empty (V2 sx sy) grid = [concat [f x y | x <- [-1..sx]] | y <- [-1..sy]]
+-- | The grid is a collection of all non-wall pieces
+type Grid = [Point]
+
+-- | A position inside the grid type
+type Point = V2 Integer
+
+-- | The grid size
+type Size = V2 Integer
+
+-- | Render the grid to a list of lines
+renderGrid :: String     -- ^ What 'String' to use to render a wall
+           -> String     -- ^ What 'String' to user to render an empty space/path
+           -> Size       -- ^ The grid size
+           -> Grid       -- ^ Input grid
+           -> [String]   -- ^ Output lines
+renderGrid wall empty (V2 sx sy) grid = [concat [renderPos (V2 x y) | x <- [-1..sx]] | y <- [-1..sy]]
   where
-    f x y= if V2 x y `elem` grid
+    -- | Get the correct 'String' for this location
+    renderPos v = if v `elem` grid
       then empty
       else wall
 
-genMaze :: V2 Integer -> StdGen -> Grid
+-- | Generat a maze of the given size
+genMaze :: Size       -- ^ The grid size
+        -> StdGen     -- ^ Random number generator
+        -> Grid       -- ^ Generated grid
 genMaze size g = snd $ execState (let p = (round <$> (fromIntegral <$> size) / 2) in iter p p) (g,[])
   where
-    iter :: V2 Integer -> V2 Integer -> MazeGen ()
+    iter :: Point -> Point -> MazeGen ()
     iter p' p = do
         (gen, !grid) <- get
-        when (valid grid size p) $ do
-            let (ndirs, gen') = shuffle dirs gen
-            put (gen', p:p':grid) 
+        when (valid grid p) $ do
+            let (ndirs, gen') = shuffle gen dirs
+            put (gen', p:p':grid)
             forM_ ndirs $ \d -> iter (p+d) (p+d*2)
-    
-dirs = [ V2   1    0 
-       , V2 (-1)   0 
-       , V2   0    1 
-       , V2   0  (-1)
-       ]
 
-valid grid size p = inside size p && p `notElem` grid  
+    -- | All four walkable directions
+    dirs :: [Point]
+    dirs = [ V2   1    0
+           , V2 (-1)   0
+           , V2   0    1
+           , V2   0  (-1)
+           ]
 
-inside (V2 sx sy) (V2 x y) = and [ x < sx, x >= 0
-                                 , y < sy, y >= 0 ]
+    -- | Check if this point is inside the grid and not already used
+    valid :: Grid -> Point -> Bool
+    valid grid p = inside p && p `notElem` grid
 
-shuffle [] gen  = ([], gen)
-shuffle xs gen  = (e:rest, gen'')
+    -- | Chick if this Point lies inside the grid
+    inside :: Point -> Bool
+    inside (V2 x y) = let V2 sx sy = size
+                      in  and [ x < sx, x >= 0
+                              , y < sy, y >= 0 ]
+
+-- | shuffle a list
+shuffle :: RandomGen g => g -> [a] -> ([a], g)
+shuffle gen [] = ([], gen)
+shuffle gen xs = (e:rest, gen'')
   where
     (i, gen')    = randomR (0, length xs - 1) gen
     (ys, e:zs)   = splitAt i xs
-    (rest,gen'') = shuffle (ys++zs) gen'
+    (rest,gen'') = shuffle gen' (ys++zs)
